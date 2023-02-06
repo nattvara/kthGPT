779ec6eda7ecca59fdc6fa2c76a9cbfe5d95e993
Noa Zetterman <noaz@kth.se>
2023-02-06 13:37:00 +0100

    Fixed support for any OS paths

--- src/transcribe/converter.py
+++ src/transcribe/converter.py
@@ -1,9 +1,9 @@
 import ffmpeg
 import uuid
-
+import os
 
 def extract_mp3(mp4: str) -> str:
-    mp3 = f'/tmp/{uuid.uuid4()}.mp3'
+    mp3 = os.path.join('tmp',f'{uuid.uuid4()}.mp3')
 
     stream = ffmpeg.input(mp4)
     stream = ffmpeg.output(stream, mp3)
