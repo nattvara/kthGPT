717114468d9f05c137aeb0bc28c427a44e8131c9
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2023-02-19 16:43:58 +0100

    update download lecture job to support youtube videos

--- jobs/download_lecture.py
+++ jobs/download_lecture.py
@@ -4,14 +4,53 @@ from rq import Queue
 import logging
 
 from tools.web.downloader import download_mp4_from_m3u8
-from db.crud import get_lecture_by_public_id_and_language, save_message_for_analysis
+from tools.youtube.download import download_mp4
 from tools.web.crawler import get_m3u8
 from db.models import Lecture, Analysis
 import jobs.download_lecture
+from db.crud import (
+    get_lecture_by_public_id_and_language,
+    save_message_for_analysis
+)
 
 
 # 20min timeout
 TIMEOUT = 20 * 60
+
+
+def download_mp4_from_kthplay(lecture: Lecture):
+    logger = logging.getLogger('rq.worker')
+
+    url = lecture.content_link()
+
+    lecture.refresh()
+    analysis = lecture.get_last_analysis()
+    analysis.mp4_progress = 2
+    analysis.save()
+
+    logger.info(f'fetching content link at {url}')
+    save_message_for_analysis(analysis, 'Downloading...', 'Looking for link to content.')
+    m3u8_url = get_m3u8(url)
+    logger.info(f'found {m3u8_url}')
+
+    lecture.refresh()
+    analysis.mp4_progress = 3
+    analysis.save()
+
+    save_message_for_analysis(analysis, 'Downloading...', 'Retrieving the recorded lecture, this is usually pretty quick.')
+    download_mp4_from_m3u8(m3u8_url, lecture)
+
+
+def download_mp4_from_youtube(lecture: Lecture):
+    url = lecture.content_link()
+
+    lecture.refresh()
+    analysis = lecture.get_last_analysis()
+    analysis.mp4_progress = 50
+    analysis.save()
+
+    save_message_for_analysis(analysis, 'Downloading...', 'Downloading the video from youtube.')
+    download_mp4(url, lecture)
 
 
 def job(lecture_id: str, language: str):
@@ -28,23 +67,12 @@ def job(lecture_id: str, language: str):
     analysis.save()
 
     try:
-        url = lecture.content_link()
-
-        lecture.refresh()
-        analysis.mp4_progress = 2
-        analysis.save()
-
-        logger.info(f'fetching content link at {url}')
-        save_message_for_analysis(analysis, 'Downloading...', 'Looking for link to content.')
-        m3u8_url = get_m3u8(url)
-        logger.info(f'found {m3u8_url}')
-
-        lecture.refresh()
-        analysis.mp4_progress = 3
-        analysis.save()
-
-        save_message_for_analysis(analysis, 'Downloading...', 'Retrieving the recorded lecture, this is usually pretty quick.')
-        download_mp4_from_m3u8(m3u8_url, lecture)
+        if lecture.source == Lecture.Source.KTH:
+            download_mp4_from_kthplay(lecture)
+        elif lecture.source == Lecture.Source.YOUTUBE:
+            download_mp4_from_youtube(lecture)
+        else:
+            raise ValueError(f'unsupported lecture source {lecture.source}')
 
         lecture.refresh()
         analysis = lecture.get_last_analysis()
@@ -72,4 +100,8 @@ if __name__ == '__main__':
         port=settings.REDIS_PORT,
         password=settings.REDIS_PASSWORD,
     ))
-    queue.enqueue(jobs.download_lecture.job, '0_blzql89t', Lecture.Language.ENGLISH)
+    queue.enqueue(
+        jobs.download_lecture.job,
+        'mn9LOFQc9IE',
+        Lecture.Language.SWEDISH
+    )
--- tools/youtube/download.py
+++ tools/youtube/download.py
@@ -1,6 +1,8 @@
 import yt_dlp
+import os
 
 from config.logger import log
+from db.models import Lecture
 
 
 def download_mp3(url: str, output_file: str) -> str:
@@ -21,3 +23,24 @@ def download_mp3(url: str, output_file: str) -> str:
         ydl.download([url])
 
     return output_file + '.mp3'
+
+
+def download_mp4(url: str, lecture: Lecture) -> str:
+    log('rq.worker').info(f'downloading {url}')
+
+    output_filename = lecture.mp4_filename()
+
+    if os.path.isfile(output_filename):
+        os.unlink(output_filename)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',
+        }],
+        'outtmpl': output_filename.replace('.mp4', '') + '.%(ext)s',
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
