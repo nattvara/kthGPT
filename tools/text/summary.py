e3662fbffa8aa42d296408804827da03d3f92c4b
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2023-02-26 16:30:28 +0100

    cache the summary to decrease the number of gpt calls

--- tools/text/summary.py
+++ tools/text/summary.py
@@ -8,6 +8,8 @@ from . import prompts, ai
 
 MIN_WORD_LIMIT = 500
 
+SUMMARY_CACHE_HIT_LIMIT = 4
+
 
 def minutes_to_time(minutes):
     hours = math.floor(minutes // 60)
@@ -72,6 +74,8 @@ class Summary:
         self.summary_size = summary_size
         self.lecture = lecture
         self.summaries = {}
+        self.cached_summary = ''
+        self.cached_summary_hits = 0
 
     def update_with_chunk(self, lecture: Lecture, chunk: Chunk, include_summary: bool):
         if lecture.language == Lecture.Language.ENGLISH:
@@ -113,6 +117,10 @@ class Summary:
         return string
 
     def summarise(self):
+        if self.cached_summary != '' and self.cached_summary_hits < SUMMARY_CACHE_HIT_LIMIT:
+            self.cached_summary_hits += 1
+            return self.cached_summary
+
         if self.lecture.language == Lecture.Language.ENGLISH:
             prompt = prompts.create_text_to_summarise_summary_english(self)
         elif self.lecture.language == Lecture.Language.SWEDISH:
@@ -141,6 +149,9 @@ class Summary:
             ],
             analysis_id=analysis.id,
         )
+
+        self.cached_summary = response
+        self.cached_summary_hits = 0
 
         return response
 
