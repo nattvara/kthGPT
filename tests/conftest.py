4763cac224ffd9ea638b637b1aa3c968866b2f7f
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2023-03-04 14:44:00 +0100

    add unit test for save_photo_from_video

---
+++ tests/conftest.py
@@ -0,0 +1,54 @@
+import subprocess
+import tempfile
+import pytest
+import os
+
+
+@pytest.fixture
+def mp4_file():
+    tf = tempfile.NamedTemporaryFile(
+        mode='w+',
+        delete=False,
+        suffix='.mp4'
+    )
+    length = 3  # length in seconds
+
+    cmd = [
+        'ffmpeg',
+        '-y',
+        '-f',
+        'lavfi',
+        '-i',
+        'testsrc=size=1920x1080:rate=1',
+        '-vf',
+        'hue=s=0',
+        '-vcodec',
+        'libx264',
+        '-preset',
+        'superfast',
+        '-tune',
+        'zerolatency',
+        '-pix_fmt',
+        'yuv420p',
+        '-t',
+        str(length),
+        '-movflags',
+        '+faststart',
+        tf.name,
+    ]
+
+    env = os.environ.copy()
+
+    process = subprocess.Popen(
+        cmd,
+        shell=False,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        env=env,
+    )
+    process.wait()
+
+    yield tf.name
+
+    tf.close()
+    os.unlink(tf.name)
