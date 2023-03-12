7bc046e0228fe1145b9ca4fde2e063d212af5848
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2023-03-12 17:24:51 +0100

    update capture_preview job to use frame from video irrespective of the source of the video

--- jobs/capture_preview.py
+++ jobs/capture_preview.py
@@ -1,45 +1,25 @@
 from config.settings import settings
 from redis import Redis
 from rq import Queue
-import logging
 
 from db.crud import get_lecture_by_public_id_and_language
-from tools.video.img import save_photo_from_video
-from tools.web.camera import save_photo
 from db.models import Lecture
 import jobs.capture_preview
+import tools.video.img
 
 # 5min timeout
 TIMEOUT = 5 * 60
 
 
 def job(lecture_id: str, language: str):
-    logger = logging.getLogger('rq.worker')
-
     lecture = get_lecture_by_public_id_and_language(lecture_id, language)
-    if lecture.raw_content_link is not None:
-        logger.info('content has raw link, using mp4 file')
-        if lecture.mp4_filepath is None:
-            raise ValueError('mp4_filepath was not specified')
 
-        mp4 = lecture.mp4_filepath
-        save_photo_from_video(mp4, lecture.preview_filename())
-        save_photo_from_video(mp4, lecture.preview_small_filename(), True)
+    if lecture.mp4_filepath is None:
+        raise ValueError('mp4_filepath was not specified')
 
-        lecture.refresh()
-        lecture.img_preview = lecture.preview_filename()
-        lecture.img_preview_small = lecture.preview_small_filename()
-        lecture.save()
-        return
-
-    url = lecture.content_link()
-    logger.info(f'taking photo of {url}')
-
-    save_photo(url, lecture)
-    logger.info(f'photo saved at {lecture.preview_filename()}')
-
-    save_photo(url, lecture, small=True)
-    logger.info(f'photo saved at {lecture.preview_small_filename()}')
+    mp4 = lecture.mp4_filepath
+    tools.video.img.save_photo_from_video(mp4, lecture.preview_filename())
+    tools.video.img.save_photo_from_video(mp4, lecture.preview_small_filename(), True)
 
     lecture.refresh()
     lecture.img_preview = lecture.preview_filename()
---
+++ tests/feature/jobs/test_capture_preview.py
@@ -0,0 +1,28 @@
+import shutil
+import os
+
+from jobs import capture_preview
+from db.models import Lecture
+
+
+def test_download_of_kth_play_lecture_saves_mp4_file(mocker, mp4_file, img_file):
+    def save_photo_from_video(mp4_filepath: str, output_file: str, small=False):
+        shutil.copy(img_file, output_file)
+
+    func = mocker.patch('tools.video.img.save_photo_from_video', side_effect=save_photo_from_video)
+
+    lecture = Lecture(
+        public_id='some_id',
+        language=Lecture.Language.SWEDISH,
+        mp4_filepath=mp4_file,
+        approved=True,
+        source=Lecture.Source.KTH,
+    )
+    lecture.save()
+
+    capture_preview.job(lecture.public_id, lecture.language)
+    lecture.refresh()
+
+    assert lecture.img_preview == lecture.preview_filename()
+    assert lecture.img_preview_small == lecture.preview_small_filename()
+    assert func.call_count == 2
