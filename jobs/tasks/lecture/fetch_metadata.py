a6432f0bbe55f56e5a3f8e0fef552da68f1d2993
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2023-04-02 23:00:54 +0200

    add feature tests for fetch_metadata lecture job

--- jobs/tasks/lecture/fetch_metadata.py
+++ jobs/tasks/lecture/fetch_metadata.py
@@ -1,11 +1,9 @@
 import logging
 
 from db.crud import get_lecture_by_public_id_and_language
-from tools.youtube.metadata import get_upload_date
-from tools.web.crawler import (
-    scrape_posted_date_from_kthplay,
-    scrape_title_from_page,
-)
+import tools.youtube.metadata
+import tools.web.crawler
+
 
 # 5 mins
 TIMEOUT = 5 * 60
@@ -20,14 +18,14 @@ def job(lecture_id: str, language: str):
 
     if lecture.source == lecture.Source.KTH:
         logger.info(f'fetching title from {lecture.content_link()}')
-        title = scrape_title_from_page(lecture.content_link())
+        title = tools.web.crawler.scrape_title_from_page(lecture.content_link())
         logger.info(f'fetching title from {lecture.content_link()}')
-        date = scrape_posted_date_from_kthplay(lecture.content_link())
+        date = tools.web.crawler.scrape_posted_date_from_kthplay(lecture.content_link())
     elif lecture.source == lecture.Source.YOUTUBE:
         logger.info(f'fetching title from {lecture.content_link()}')
-        title = scrape_title_from_page(lecture.content_link())
+        title = tools.web.crawler.scrape_title_from_page(lecture.content_link())
         logger.info(f'fetching title from {lecture.content_link()}')
-        date = get_upload_date(lecture.content_link())
+        date = tools.youtube.metadata.get_upload_date(lecture.content_link())
     elif lecture.source == lecture.Source.KTH_RAW:
         logger.warning('kth_raw does not support metadata')
         return
