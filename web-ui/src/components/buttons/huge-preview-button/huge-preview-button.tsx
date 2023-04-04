7afeddbba604fdd3d69c9a27934bd2ef02f2ffad
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2023-04-05 01:02:19 +0200

    add HugePreviewButton that is a huge button that shows a preivew of a html element

---
+++ web-ui/src/components/buttons/huge-preview-button/huge-preview-button.tsx
@@ -0,0 +1,50 @@
+import styles from './huge-preview-button.less';
+import { Row, Typography } from 'antd';
+import { useState } from 'react';
+import { history } from 'umi';
+
+const { Title } = Typography;
+
+interface HugePreviewButtonProps {
+  icon: JSX.Element;
+  title: string;
+  subtitle: string;
+  url: string;
+  preview: JSX.Element;
+}
+
+export default function HugePreviewButton(props: HugePreviewButtonProps) {
+  const [hovering, setHovering] = useState<boolean>(false);
+
+  const goToUrl = async (url: string, newTab = false) => {
+    if (newTab) {
+      window.open(url, '_blank');
+    } else {
+      await history.push(url);
+    }
+  };
+
+  return (
+    <div
+      className={styles.container}
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
+      onClick={(e: React.MouseEvent) => {
+        if (e.metaKey) return goToUrl(props.url, true);
+        if (e.ctrlKey) return goToUrl(props.url, true);
+        goToUrl(props.url);
+      }}
+    >
+      <Row className={`${styles.icon} ${hovering ? styles.hovering : ''}`}>
+        {props.icon}
+      </Row>
+      <Row className={`${styles.title} ${hovering ? styles.hovering : ''}`}>
+        <Title level={3}>{props.title}</Title>
+      </Row>
+      <Row className={`${styles.subtitle} ${hovering ? styles.hovering : ''}`}>
+        <Title level={5}>{props.subtitle}</Title>
+      </Row>
+      <div className={styles.background}>{props.preview}</div>
+    </div>
+  );
+}
