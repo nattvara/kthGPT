2ce4ca83f15b51c7126f37107f4cdee990937c57
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2023-04-08 17:59:58 +0200

    add ImageUpload component that allows a user to create an image upload

---
+++ web-ui/src/components/image/image-upload/image-upload.tsx
@@ -0,0 +1,151 @@
+import styles from './image-upload.less';
+import { Row, Col, Alert, Upload, Image, Typography, notification } from 'antd';
+import type { UploadProps } from 'antd';
+import { useMutation } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import apiClient, {
+  makeUrl,
+  ServerErrorResponse,
+  ServerResponse,
+} from '@/http';
+import { FileImageOutlined } from '@ant-design/icons';
+import { UploadChangeParam, UploadFile } from 'antd/es/upload';
+import { Image as ImageType } from '@/types/search';
+
+const { Title } = Typography;
+const { Dragger } = Upload;
+
+interface ImageResponse extends ServerResponse {
+  data: ImageType;
+}
+
+interface ImageUploadProps {
+  uploadId?: string;
+  noMargin?: boolean;
+  onUploadComplete: (image: ImageType) => void;
+}
+
+export default function ImageUpload(props: ImageUploadProps) {
+  const { uploadId, onUploadComplete, noMargin } = props;
+
+  const [id, setId] = useState<null | string>(null);
+  const [error, setError] = useState<string>('');
+  const [hovering, setHovering] = useState<boolean>(false);
+  const [image, setImage] = useState<null | ImageType>(null);
+  const [notificationApi, contextHolder] = notification.useNotification();
+
+  const previewUrl = makeUrl(`/assignments/image/${id}/img`);
+
+  const uploadProps: UploadProps = {
+    name: 'file',
+    multiple: false,
+    action: makeUrl('/assignments/image'),
+    maxCount: 1,
+    async onChange(info: UploadChangeParam<UploadFile>) {
+      const { status } = info.file;
+      if (status === 'done') {
+        setId(info.file.response.id);
+        setError('');
+      } else if (status === 'error') {
+        if (info.file.response) {
+          setError(info.file.response.detail);
+        } else {
+          setError(
+            'Something went wrong when uploading the image. Please try again.'
+          );
+        }
+      }
+    },
+  };
+
+  const { mutate: fetchImage } = useMutation(
+    async () => {
+      return await apiClient.get(`/assignments/image/${id}`);
+    },
+    {
+      onSuccess: (res: ImageResponse) => {
+        const result = {
+          status: res.status + '-' + res.statusText,
+          headers: res.headers,
+          data: res.data,
+        };
+        setImage(result.data);
+        onUploadComplete(result.data);
+      },
+      onError: (err: ServerErrorResponse) => {
+        notificationApi['error']({
+          message: 'Failed to get image',
+          description: err.response.data.detail,
+        });
+      },
+    }
+  );
+
+  useEffect(() => {
+    if (uploadId === undefined) {
+      return;
+    }
+    setId(uploadId);
+  }, [uploadId]);
+
+  useEffect(() => {
+    if (id !== null) fetchImage();
+  }, [id, fetchImage]);
+
+  return (
+    <div
+      className={styles.image_upload}
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
+    >
+      {contextHolder}
+      <Row>
+        <Dragger
+          className={`${styles.dragger} ${noMargin ? styles.no_margin : ''}`}
+          {...uploadProps}
+        >
+          {(id === null || image === null) && (
+            <>
+              <Row
+                className={`${styles.icon} ${hovering ? styles.hovering : ''}`}
+              >
+                <FileImageOutlined />
+              </Row>
+              <Row
+                className={`${styles.title} ${hovering ? styles.hovering : ''}`}
+              >
+                <Title level={3}>Ask a question about an assignment</Title>
+              </Row>
+              <Row
+                className={`${styles.subtitle} ${
+                  hovering ? styles.hovering : ''
+                }`}
+              >
+                <Title level={5}>
+                  Click or drag image of an assignment to this area to upload
+                </Title>
+              </Row>
+            </>
+          )}
+          {!(id === null || image === null) && (
+            <Image height={200} src={previewUrl} preview={false} />
+          )}
+        </Dragger>
+      </Row>
+      <Row>
+        {error !== '' && (
+          <Row justify="center">
+            <Col>
+              <Alert
+                message="The image was not accepted"
+                description={error}
+                type="error"
+                showIcon
+              />
+            </Col>
+          </Row>
+        )}
+      </Row>
+    </div>
+  );
+}
