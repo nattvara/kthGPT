69f8f445de5c1951fb1a3b5183f077d1bd70d891
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2023-04-22 16:20:07 +0200

    add 'I'm feeling lucky button for assignments to index page

--- web-ui/src/pages/index.tsx
+++ web-ui/src/pages/index.tsx
@@ -2,17 +2,24 @@ import { Lecture } from '@/types/lecture';
 import PageFrame from '@/components/page/page-frame/page-frame';
 import SearchHuge from '@/components/search/search-huge/search-huge';
 import { registerPageLoad } from '@/matomo';
-import { Col, Row } from 'antd';
+import { Button, Col, Row } from 'antd';
 import styles from './index.less';
 import { useEffect, useState } from 'react';
 import SearchResult from '@/components/search/search-results/search-result';
 import ButtonHuge from '@/components/button/button-huge/button-huge';
-import { FileSearchOutlined, VideoCameraAddOutlined } from '@ant-design/icons';
+import {
+  BulbOutlined,
+  FileSearchOutlined,
+  VideoCameraAddOutlined,
+} from '@ant-design/icons';
 import ButtonHugeWithPreview from '@/components/button/button-huge-with-preview/button-huge-with-preview';
+import apiClient from '@/http';
 import CourseList from '@/components/course/course-list/course-list';
 import ImageUpload from '@/components/image/image-upload/image-upload';
 import { Image } from '@/types/search';
 import { history } from 'umi';
+
+const RANDOM_ASSIGNMENT_SUBJECT = 'Mathematics';
 
 export default function IndexPage() {
   const [lectures, setLectures] = useState<Lecture[]>([]);
@@ -24,6 +31,18 @@ export default function IndexPage() {
 
   const onImageUploadComplete = (image: Image) => {
     history.push(`/assignments/${image.id}`);
+  };
+
+  const goToRandomAssignment = async () => {
+    try {
+      const response = await apiClient.get(
+        `/assignments/image/random/${RANDOM_ASSIGNMENT_SUBJECT}`
+      );
+      const id = response.data.id;
+      history.push(`/assignments/${id}`);
+    } catch (err: unknown) {
+      console.error(err);
+    }
   };
 
   useEffect(() => {
@@ -55,6 +74,17 @@ export default function IndexPage() {
                 <ImageUpload
                   onUploadComplete={(image) => onImageUploadComplete(image)}
                 />
+                <Row justify="center">
+                  <Col>
+                    <Button
+                      onClick={() => goToRandomAssignment()}
+                      type="primary"
+                      size="large"
+                    >
+                      <BulbOutlined /> Look at a random assignment
+                    </Button>
+                  </Col>
+                </Row>
               </Col>
               <Col sm={24} md={8}>
                 <ButtonHugeWithPreview
